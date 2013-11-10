21ddb167684a12260c4736adbd2fa2d3528f406a
Brett Sanders <brettrsanders@gmail.com>
2013-11-10 00:14:19 -0600

    refactor width, height into vars; add background color

--- gosu_game_of_life.rb
+++ gosu_game_of_life.rb
@@ -5,15 +5,29 @@ require_relative 'game_of_life.rb'
 
 class GameOfLifeWindow < Gosu::Window
 
-  def initialize
-    super 800, 600, false
+  def initialize(height=800, width=600)
+    @height = height
+    @width = width
+    super height, width, false
     self.caption = "Our game of life"
+
+    # colors
+    @background_color = Gosu::Color.new(0xffdedede)
   end
 
   def update
+    # updates game 60x / sec
+    # logic for updating goes here, but our logic is in game_of_life.rb
   end
 
   def draw
+    # tells gosu gaming library how to give color, style
+
+    # rectangle background
+    draw_quad(0, 0, @background_color,
+              width, 0, @background_color,
+              width, height, @background_color,
+              0, height, @background_color)
   end
 
   def needs_cursor?; true; end
